443bb24361525b85be60053a9d743292d5c899a7
patientLink <pc353240229@gmail.com>
2020-06-15 00:22:08 +0800

    change route

---
+++ build/precache-manifest.defba722ae80e3391a514ad671df20c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b65ff8cd50417e97e515e17c7ff9340",
+    "url": "/cnode-react/index.html"
+  },
+  {
+    "revision": "b08aa2d613643e8b8b41",
+    "url": "/cnode-react/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "b190d9e9ca55c5e0f1ee",
+    "url": "/cnode-react/static/css/main.5cdcec71.chunk.css"
+  },
+  {
+    "revision": "b08aa2d613643e8b8b41",
+    "url": "/cnode-react/static/js/2.9baf3570.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cnode-react/static/js/2.9baf3570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b190d9e9ca55c5e0f1ee",
+    "url": "/cnode-react/static/js/main.a281c6ad.chunk.js"
+  },
+  {
+    "revision": "012e74c9633739001f30",
+    "url": "/cnode-react/static/js/runtime-main.57a93798.js"
+  },
+  {
+    "revision": "86f6a7c2f6cf9844d8a9f5d088d87406",
+    "url": "/cnode-react/static/media/cnodejs_light.86f6a7c2.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/patientLink/cnode-react/precache-manifest.5490ca28a139c25499fc57b4a8eff441.js"
+  "/cnode-react/precache-manifest.defba722ae80e3391a514ad671df20c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/patientLink/cnode-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cnode-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
